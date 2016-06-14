ea8a5ecbbbbaf450934eba93ba0d5e7101bdd4e7
Automaat <marcin0skalski@gmail.com>
2016-06-14 22:06:28 +0200

    introduced small changes regarding config server

--- src/main/java/com/automaat/boundry/web/ResourceController.java
+++ src/main/java/com/automaat/boundry/web/ResourceController.java
@@ -3,6 +3,8 @@ package com.automaat.boundry.web;
 import com.automaat.model.SimpleResource;
 import com.automaat.repositories.SimpleResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,15 @@ import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@RefreshScope
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 public class ResourceController {
 
     private final SimpleResourceRepository repository;
+
+    @Value("${message}")
+    private String message;
 
     @Autowired
     public ResourceController(SimpleResourceRepository repository) {
@@ -35,5 +41,10 @@ public class ResourceController {
     @RequestMapping(value = "/resources", method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Collection<SimpleResource>> findAll(){
         return new ResponseEntity<>(repository.findAll(), OK);
+    }
+
+    @RequestMapping(value = "/message", method = GET)
+    public String testingConfig(){
+        return this.message;
     }
 }
